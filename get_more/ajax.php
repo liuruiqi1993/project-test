2f32bc92ede6b649e4f2ea3ce9986684851efbeb
liuruiqi1993 <33003931+liuruiqi1993@users.noreply.github.com>
2018-07-16 11:23:22 +0800

    Update ajax.php

--- get_more/ajax.php
+++ get_more/ajax.php
@@ -1,8 +1,7 @@
 <?php
-//echo "jinlaile"
     $str = file_get_contents("info.json");
     $arr = json_decode($str);
-    /*一次多张*/
+    /*MORE THAN ONE*/
     $rand = array_rand($arr,2);
     for($i=0;$i<count($rand);$i++){
         $obj[$i] = $arr[$rand[$i]];
