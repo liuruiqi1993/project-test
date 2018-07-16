c82c63f239afdd9cd1a4be1c9f7b99046d400f58
liuruiqi1993 <33003931+liuruiqi1993@users.noreply.github.com>
2018-07-16 12:01:23 +0800

    Update ajax.php

--- get_more/ajax.php
+++ get_more/ajax.php
@@ -1,16 +1,8 @@
 <?php
-<<<<<<< HEAD
     $str = file_get_contents("info.json");
     $arr = json_decode($str);
     /*more than one*/
     $rand = array_rand($arr,5);
-=======
-    $header[] = "Content-type: application/x-www-form-urlencoded";
-    $str = file_get_contents("info.json");
-    $arr = json_decode($str);
-    /*MORE THAN ONE*/
-    $rand = array_rand($arr,2);
->>>>>>> 43d1374d5d892b507da2270cc1c4cfd5808cb833
     for($i=0;$i<count($rand);$i++){
         $obj[$i] = $arr[$rand[$i]];
     }
