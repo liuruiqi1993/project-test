edd30bf6cb7be4930f4b6cf02d9ac1fc1e594e08
liuruiqi1993 <405782134@qq.com>
2018-07-16 11:59:11 +0800

    update

--- get_more/ajax.php
+++ get_more/ajax.php
@@ -1,9 +1,8 @@
 <?php
-//echo "jinlaile"
     $str = file_get_contents("info.json");
     $arr = json_decode($str);
-    /*一次多张*/
-    $rand = array_rand($arr,2);
+    /*more than one*/
+    $rand = array_rand($arr,5);
     for($i=0;$i<count($rand);$i++){
         $obj[$i] = $arr[$rand[$i]];
     }
